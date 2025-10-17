f81783b34130c548df59b0ffcd4666bd5d65c2a0
MrNikhillYadav <facts.foundr@gmail.com>
2025-10-17 20:33:40 +0530

    add class

---
+++ oct_17/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { Counter } from './components/Counter'
+
+export default function App() {
+ return (
+    <div>
+      <Counter/>       
+    </div>
+ )
+}
+
---
+++ oct_17/src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export function Counter(){
+     const [count,setCount] = useState(0);
+
+    function handleIncrement(){
+        setCount(count +1);
+    }
+    
+    return <div>
+        <h1>Count :{count} </h1>
+        <button onClick={handleIncrement}>Increment</button>
+    </div>
+}
