7ee62fb9ace444bd4ef0e48b5a578c802b0a8b62
Nikhil Yadav <facts.foundr@gmail.com>
2025-10-27 19:46:24 +0530

    add props

---
+++ oct_27/src/App.jsx
@@ -0,0 +1,15 @@
+import { Counter } from './components/Counter'
+import './App.css'
+import MouseHoverDemo from './components/MouseHover'
+import PropsPassingDemo from './components/PropsPassingDemo'
+
+function App() {
+  return (
+    <>
+      {/* <Counter/> */}
+      {/* <MouseHoverDemo/> */}
+      <PropsPassingDemo/>
+    </>
+  )
+}
+export default App
